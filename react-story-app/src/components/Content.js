e2c9e2cf9ceaf53bf9dc280fc9ca716eb712a7a9
Gabriela-707 <gabriela_godfrey@outlook.com>
2025-02-01 04:43:49 -0500

    Initial commit with React app and story components

---
+++ react-story-app/src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content() {
+    return (
+        <main>
+            <p>The story is about a young boy who forms a close bond with a tree in a forest. The tree loves the boy and is happy to provide for him, offering her branches, leaves, and even her trunk for him to use as he grows up. As a child, the boy enjoys playing in the tree’s shade, climbing her branches, and swinging from them. Over time, as the boy matures into an adult, his needs change, and he asks the tree for more and more, each time taking more than she has to offer. The tree gives selflessly, offering her branches to build a house, her trunk to make a boat, and even her stump for the boy to sit on when he is old and weary.</p>
+            <p>In the end, the boy is now an elderly man and returns to the tree, who is now just a stump. The tree, even though she has given him everything, is still happy to offer what little she has left—providing a place for him to rest. The story shows the tree’s selfless love, generosity, and sacrifice, as the tree continually gives of herself to the boy, even as he takes without ever giving back.</p>
+        </main>
+    );
+}
+
+export default Content;
---
+++ react-story-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p><strong>Moral of the Story:</strong> Selflessness and generosity can sometimes be taken too far. Balance and self-care is important in any relationship</p>
+        </footer>
+    );
+}
+
+export default Footer;
